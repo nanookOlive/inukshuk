19b35ba1b32f8c61003385ff2b9096bbbac291ef
nanookOlive <olivier.stierer@gmail.com>
2023-09-22 10:47:45 +0200

    new design for Auteur view lsit

--- app/view/Auteur.php
+++ app/view/Auteur.php
@@ -1,5 +1,6 @@
 
-
+<div class='list-auteur'>
 <?php foreach($data as $auteur):?>
-    <a href="<?= $router->generate('byAuteur',['id'=>$auteur->getId()])?>"><?= $auteur->getAuteur()?></a>
+    <a href="<?= $router->generate('byAuteur',['id'=>$auteur->getId()])?>" class='auteur-click'><?= $auteur->getAuteur()?></a>
 <?php endforeach?>
+</div>
