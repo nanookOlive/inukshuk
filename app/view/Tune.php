b3dfa48d1e8157f813a3a03fdcac5f9280957a18
nanookOlive <olivier.stierer@gmail.com>
2023-09-25 07:48:02 +0200

    fix name tune and add projetc text

--- app/view/Tune.php
+++ app/view/Tune.php
@@ -3,7 +3,7 @@
 
 
         <?php foreach($data as $grille):?>
-            <p class='item-click' value='<?=$grille->getTitre()?>'><?= $grille->getTitre()?></p> <!--implementer une fonction rand qui affiche un cara inuktitut devant chaque item -->
+            <p class='item-click' value="<?=$grille->getTitre()?>"><?= $grille->getTitre()?></p> <!--implementer une fonction rand qui affiche un cara inuktitut devant chaque item -->
             <span class='underline-item-click'></span>
             <?php endforeach?>
 
