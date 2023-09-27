886f3c7332b7607a28c69f01ed1f44f25b5559f5
nanookOlive <olivier.stierer@gmail.com>
2023-09-27 16:14:46 +0200

    sending ok but not to gmail

--- core/Utils.php
+++ core/Utils.php
@@ -6,9 +6,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require_once '/mvc2/src/Exception.php';
-require_once '/mvc2/src/PHPMailer.php';
-require_once '/mvc2/src/SMTP.php';
+require_once '../src/Exception.php';
+require_once '../src/PHPMailer.php';
+require_once '../src/SMTP.php';
 
 
 
@@ -17,5 +17,37 @@ require_once '/mvc2/src/SMTP.php';
 function sendRequestInscription(){
 
     $mail = new PHPMailer();
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host= 'mail.gandi.net';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username='inukshuk@nanookpandora.com';
+        $mail->Password='inukshuk!';
+                          //SMTP password
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+        //Recipients
+        $mail->setFrom('inukshuk@nanookpandora.com', 'Mailer');
+        $mail->addAddress('olivier.stierer@gmail.com', 'Olivier');     //Add a recipient
+       
+    
+      
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Salut Raton';
+        $mail->Body    = 'Vous êtes une saucisse chere amie !';
+    
+        $mail->send();
+
+
+    } catch (Exception $e) {
+
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+
+
 
 }
