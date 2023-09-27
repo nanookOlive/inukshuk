638bf75aabe0d0a6bfaf219b1343e60bc6963552
nanookOlive <olivier.stierer@gmail.com>
2023-09-27 14:53:27 +0200

    add Utils file in core for function util

---
+++ core/Utils.php
@@ -0,0 +1,21 @@
+<?php
+
+///biblio de fonctions utiles
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once '/mvc2/src/Exception.php';
+require_once '/mvc2/src/PHPMailer.php';
+require_once '/mvc2/src/SMTP.php';
+
+
+
+///envoi de mail
+
+function sendRequestInscription(){
+
+    $mail = new PHPMailer();
+
+}
