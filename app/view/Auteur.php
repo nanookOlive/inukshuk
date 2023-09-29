84fb8076583d5c92d9bb38c4656225dd1f85a2c9
nanookOlive <olivier.stierer@gmail.com>
2023-09-29 16:14:12 +0200

    change place of auteur list in auteur

--- app/view/Auteur.php
+++ app/view/Auteur.php
@@ -1,6 +1,17 @@
 
-<div class='list-auteur'>
-<?php foreach($data as $auteur):?>
-    <a href="<?= $router->generate('byAuteur',['id'=>$auteur->getId()])?>" class='auteur-click'><?= $auteur->getAuteur()?></a>
-<?php endforeach?>
+
+<div class='mama-flex'>
+
+    <div id='liste-grille'>
+
+
+        <?php foreach($data as $auteur):?>
+            <a href="<?= $router->generate('byAuteur',['id'=>$auteur->getId()])?>" class='auteur-click'><?=$auteur->getAuteur()?></a>
+            <span class='underline-item-click'></span>
+            <?php endforeach?>
+
+    </div>
+    
+    
+
 </div>
